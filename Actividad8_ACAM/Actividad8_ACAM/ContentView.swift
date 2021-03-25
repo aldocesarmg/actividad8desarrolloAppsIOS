d4b7eb88389cfa9ad6ddbb97c5d7cf85a187e568
aldocesarmg <aldocesarmg@gmail.com>
2021-03-24 19:47:50 -0600

    Actividad 8

---
+++ Actividad8_ACAM/Actividad8_ACAM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Actividad8_ACAM
+//
+//  Created by user183935 on 3/24/21.
+//  Copyright © 2021 aldocesarmg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Actividad8_ACAM/Actividad8_ACAMTests/Actividad8_ACAMTests.swift
@@ -0,0 +1,34 @@
+//
+//  Actividad8_ACAMTests.swift
+//  Actividad8_ACAMTests
+//
+//  Created by user183935 on 3/24/21.
+//  Copyright © 2021 aldocesarmg. All rights reserved.
+//
+
+import XCTest
+@testable import Actividad8_ACAM
+
+class Actividad8_ACAMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
